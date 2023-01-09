808d52013a8c7041c82369d61875609d98a83b54
reda <reda.himmi@edu.uca.ma>
2023-01-09 05:01:00 +0100

    added recording from mic

--- src/main/java/com/ensa/videots/SpeechRecognitionController.java
+++ src/main/java/com/ensa/videots/SpeechRecognitionController.java
@@ -5,10 +5,12 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
+import org.w3c.dom.ls.LSOutput;
 
 import javax.sound.sampled.LineUnavailableException;
 
@@ -20,14 +22,24 @@ public final class SpeechRecognitionController{
     private Label time;
     @FXML
     private ChoiceBox<String> choice;
+    @FXML
+    private Button start;
+
+    @FXML
+    private Button end;
+
+    @FXML
+    private Label errorMsgLabel;
 
     private Chronometer chrono;
 
     private Timeline timeline;
 
-    private String[] languages = {"English","French","Spanish","Arabic"};
+    private String[] languages = {"English","French","Spanish"};
 
     private AudioRecorder audioRecorder = new AudioRecorder();
+
+    private boolean state = true;
 
     public SpeechRecognitionController() throws LineUnavailableException {
         chrono   = new Chronometer();
@@ -38,18 +50,28 @@ public final class SpeechRecognitionController{
     private void initialize() {
         time.textProperty().bind(Bindings.format("%02d:%02d:%02d:%d%d", chrono.hh, chrono.mm, chrono.ss, chrono.th, chrono.hd));
         choice.getItems().addAll(languages);
-
+        end.setVisible(false);
     }
+
     @FXML
     private void start() throws LineUnavailableException {
-        timeline.play();
-        audioRecorder.begin();
+        errorMsgLabel.setText("");
+        if (choice.getValue() != null) {
+            timeline.play();
+            audioRecorder.begin();
+            start.setVisible(false);
+            end.setVisible(true);
+        }else {
+            errorMsgLabel.setText("Please select a language !");
+        }
     }
     @FXML
     private void stop() {
         timeline.stop();
         chrono.reset();
         audioRecorder.end();
+        start.setVisible(true);
+        end.setVisible(false);
+        choice.setValue(null);
     }
-
 }
